2b04fd4f2b6dc179986adab1f562c8854bc9bc85
annams <annaextrasteckel@gmail.com>
2025-06-01 10:22:36 -0700

    form submission is functioning

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Garden
 from django.views.generic.edit import CreateView
 from .forms import GardenForm
@@ -11,3 +11,9 @@ def home(request):
 class GardenCreate(CreateView):
     model = Garden
     form_class = GardenForm
+    success_url = '/profile/'
+    
+    # Adding this due to null error upon form submission. This makes sure to override the created_by relationship field and assign it automatically to the user. https://docs.djangoproject.com/en/5.2/topics/class-based-views/generic-editing/
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
