29d1c1cfa06918dc17d02d77e4ff1bd403f06f55
annams <annaextrasteckel@gmail.com>
2025-06-01 20:53:48 -0700

    user survey functioning with exprience level and basic styling; need to get location data set up next

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from .models import Garden
-from django.views.generic.edit import CreateView
-from .forms import GardenForm
+from .models import Garden, User
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import GardenForm, UserForm
 
 # Create your views here.
 def home(request):
@@ -10,6 +10,15 @@ def home(request):
 
 def start(request):
     return render(request, 'main_app/start.html')
+
+class UserUpdate(UpdateView):
+    model = User
+    form_class = UserForm
+    success_url = '/'
+    
+    # Update only the logged-in user.
+    def get_object(self):
+        return self.request.user
 
 class GardenCreate(CreateView):
     model = Garden
@@ -20,3 +29,4 @@ class GardenCreate(CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+    
