37493330bad4375713e0f2803441879f8ac5dbcd
annams <annaextrasteckel@gmail.com>
2025-06-12 15:44:10 -0700

    ongoing deployment debugging

--- freshfutures/settings.py
+++ freshfutures/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 import dj_database_url
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
