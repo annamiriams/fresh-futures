ec27d9b78f4ad906b676f0eb5eed6e826f3a1b92
annams <annaextrasteckel@gmail.com>
2025-06-12 17:00:04 -0700

    ongoing deployment debugging

--- freshfutures/wsgi.py
+++ freshfutures/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freshfutures.settings')
+os.environ['GDAL_LIBRARY_PATH'] = '/usr/lib/libgdal.so'
 
 application = get_wsgi_application()
